b70d8e7e2115387ce2e76ce01d5117b4ab0a534d
Ilo Tech <ilo.nikatsadze@gmail.com>
2024-03-08 19:19:32 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { evaluate,format } from "mathjs";
+
 
 function App() {
+
+  const [Value,SetValue]=React.useState("");
+
+ const eq=/^([(]*\s*\d+(\.\d+)?)(\s*[)]*\s*[-*/+]\s*[(]*\s*[-]?\s*\d+(\.\d+)?\s*[)]*)*$/;
+ const eq_2=/\d*/;
+ function calculate(){
+ if(eq.test(Value)||eq_2.test(Value)){
+  try{
+  let result=evaluate(Value);
+  if(result===Infinity||result===(-Infinity)){
+      SetValue("can't divide by zero");
+  }
+  else{
+      SetValue(`${format(result,{precision:14})}`)
+      console.log(result)
+  }
+  
+  }
+  catch{
+  SetValue("Invalid expression")
+  }
+
+}
+else{
+  SetValue("Invalid expression")
+}
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+
+    <div className="eq_container">
+    <input className="expression" type="text" value={Value} readOnly /> 
     </div>
+
+    <div className="calculator">
+    
+
+    <div className="numbers">
+      <button onClick={()=>SetValue(prew=>prew+"1")}>1</button>
+      <button onClick={()=>SetValue(prew=>prew+"2")}>2</button>
+      <button onClick={()=>SetValue(prew=>prew+"3")}>3</button>
+      <button onClick={()=>SetValue(prew=>prew+"4")}>4</button>
+      <button onClick={()=>SetValue(prew=>prew+"5")}>5</button>
+      <button onClick={()=>SetValue(prew=>prew+"6")}>6</button>
+      <button onClick={()=>SetValue(prew=>prew+"7")}>7</button>
+      <button onClick={()=>SetValue(prew=>prew+"8")}>8</button>
+      <button onClick={()=>SetValue(prew=>prew+"9")}>9</button>
+      <button onClick={()=>SetValue(prew=>prew+".")}>.</button>
+      <button onClick={()=>SetValue(prew=>prew+"0")}>0</button>
+      <button onClick={calculate}>=</button>
+    </div>
+
+    <div className="signs">
+      <button onClick={()=>SetValue(prew=>prew+"+")}>+</button>
+      <button onClick={()=>SetValue(prew=>prew+"-")}>-</button>
+      <button onClick={()=>SetValue(prew=>prew+"*")}>*</button>
+      <button onClick={()=>SetValue(prew=>prew+"/")}>/</button>
+      <button onClick={()=>SetValue(prew=>prew+"(")}>&#40;</button>
+      <button onClick={()=>SetValue(prew=>prew+")")}>&#41;</button>
+      <button onClick={()=>SetValue(Value.slice(0,-1))}>&lt;</button>
+      <button onClick={()=>SetValue("")}>C</button>
+    </div>
+
+    </div>
+  </div>
+
   );
 }
 
